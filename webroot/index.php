398e90b8ef4a5c67882ca067a66d512182c20722
Phil Weir <phil.weir@numa.ie>
2015-07-21 20:57:26 +0100

    correct vendor location

--- webroot/index.php
+++ webroot/index.php
@@ -63,7 +63,7 @@ define('TMP', ROOT . DS . 'tmp' . DS);
  * The following line differs from its sibling
  * /lib/Cake/Console/Templates/skel/webroot/index.php
  */
-define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'vendor' . DS . 'cakephp' . DS . 'cakephp' . DS . 'lib');
+define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'Vendor' . DS . 'cakephp' . DS . 'cakephp' . DS . 'lib');
 
 /**
  * Editing below this line should NOT be necessary.
